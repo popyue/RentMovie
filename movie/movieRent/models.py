8c34378d0564b6af6e150384ddd51d5bda11e00b
popyue <l20382006@gmail.com>
2020-10-12 18:08:20 +0800

    Add Basic HTML File

--- movie/movieRent/models.py
+++ movie/movieRent/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
+	category_id = models.PositiveIntegerField(default=0)
 	category_name = models.CharField(max_length=20, default="Action")
 	item_num = models.PositiveIntegerField(default=1)
 
@@ -10,14 +11,17 @@ class Category(models.Model):
 		return self.category_name
 
 class MovieProduct(models.Model):
-	categorye = models.ForeignKey(Category, on_delete=models.CASCADE)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	movie_name = models.CharField(max_length=20, default="Fast Frious")
 	movie_describe = models.TextField(max_length=100)
 	movie_score = models.FloatField(default= 10.0)
 	in_stock = models.PositiveIntegerField(default=0)
 	price = models.FloatField(default=500.0)
 	picture = models.ImageField(upload_to=None)
-	pub_date = models.DateTimeField('date published')
+	videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
+	pub_date = models.DateTimeField('date published', default=None)
 
 	def __str__(self):
 		return self.movie_name
+
+
--- movie/movieRent/views.py
+++ movie/movieRent/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, MovieProduct
+
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You're at the Movie Rent Store index.")
-
+	#return HttpResponse("Hello, world. You're at the Movie Rent Store index.")
+	return render(request, 'index.html')
 
 def movie_content(request, movie_id):
 	content = "This site use to display the detail of %s's movie!!"
