98d326bcdd3bfc9be364de2250dd29f0acfcc025
popyue <l20382006@gmail.com>
2020-10-13 19:16:37 +0800

    202010113 upload

--- movie/movieRent/models.py
+++ movie/movieRent/models.py
@@ -3,25 +3,48 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-	category_id = models.PositiveIntegerField(default=0)
+	#category_id = models.PositiveIntegerField(default=0)
 	category_name = models.CharField(max_length=20, default="Action")
 	item_num = models.PositiveIntegerField(default=1)
-
+	slug = models.SlugField(max_length=200,
+		db_index=True,
+		unique=True)
+	class Meta:
+		ordering = ('category_name',)
+		verbose_name = 'category'
+		verbose_name_plural = 'categories'
+	
 	def __str__(self):
 		return self.category_name
+	
+	def get_absolute_url(self):
+		return reverse('movieRent:product_list_by_category',
+			args=[self.slug])
+
 
 class MovieProduct(models.Model):
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	category = models.ForeignKey(
+			Category,
+			related_name='movieproducts', 
+			on_delete=models.CASCADE)
 	movie_name = models.CharField(max_length=20, default="Fast Frious")
-	movie_describe = models.TextField(max_length=100)
+	slug = models.SlugField(max_length=200, db_index=True)
+	movie_describe = models.TextField(max_length=1000)
 	movie_score = models.FloatField(default= 10.0)
 	in_stock = models.PositiveIntegerField(default=0)
 	price = models.FloatField(default=500.0)
-	picture = models.ImageField(upload_to=None)
-	videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
-	pub_date = models.DateTimeField('date published', default=None)
+	picture = models.ImageField(upload_to='static/movie_pic/', null=True)
+	videofile= models.FileField(upload_to='static/videos/', null=True, verbose_name="")
+	pub_date = models.DateTimeField(auto_now_add=True)
 
+	class Meta:
+		ordering = ('movie_name',)
+		index_together = (('id', 'slug'),)
+	
 	def __str__(self):
 		return self.movie_name
 
+	def get_absolute_url(self):
+		return reverse('shop:product_detail',
+        	args=[self.id, self.slug])
 
--- movie/movieRent/views.py
+++ movie/movieRent/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views import generic
 from .models import Category, MovieProduct
 
 
 # Create your views here.
-def index(request):
+def index(request, category_slug=None):
 	#return HttpResponse("Hello, world. You're at the Movie Rent Store index.")
-	return render(request, 'index.html')
+	category = None
+	categories = Category.object.all()
+	movie = MovieProduct.object.filter(available=True)
+	if category_slug:
+		category = get_object_or_404(Category, slug=category_slug)
+		movie = movie.filter(category=category)
+	return render(request, 
+		'movieRent/templates/index.html',
+		{ 
+		'category' : category,
+		'categories': categories,
+		'movie': movie 
+		} )
 
-def movie_content(request, movie_id):
-	content = "This site use to display the detail of %s's movie!!"
-	return HttpResponse(content % movie_id)
+def movie_content(request, movie_id, slug):
+	#content = "This site use to display the detail of %s's movie!!"
+	movie = get_object_or_404(MovieProduct,
+		id=movie_id,
+		slug=slug,
+		available=True)
 
-def category_content(request, category_id):
-	content = "This is a list view for %s's category content"
-	return HttpResponse(content % category_id)
+	return render(request,
+		'movieRent/templates/detail.html')
 
-
-def rentcar(request):
-	response = "This is the rent car before payment"
-	return HttpResponse(response)
